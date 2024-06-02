2fa466dc5cc9a72fc52ffc37ec466c5078badaa3
BBongDDa2 <54784931+BBongDDa@users.noreply.github.com>
2024-06-02 23:17:29 +0900

    fix : 디지털 안내사 지도영역 짤리는 이슈 ->  높이 100%로 수정

--- src/pages/helper_map/neary_by.js
+++ src/pages/helper_map/neary_by.js
@@ -155,7 +155,7 @@ export default function Main() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar title="디지털 안내사 위치 보기" link="#"></NavBar>
-      <div className="flex flex-col items-center justify-center w-full h-[calc(100%-48px)] bg-[#232323]">{isScriptLoaded && <div id="map" style={{ width: "100%", height: "800px" }}></div>}</div>
+      <div className="flex flex-col items-center justify-center w-full h-[calc(100%-48px)] bg-[#232323]">{isScriptLoaded && <div id="map" style={{ width: "100%", height: "100%" }}></div>}</div>
     </>
   );
 }
